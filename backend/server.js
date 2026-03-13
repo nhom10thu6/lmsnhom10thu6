fbfe928567a8b1b42bd7f005892ebc5a9aa4824e
LamCongDanh <taigaxda@gmail.com>
2026-03-13 16:24:56 +0700

    update backend comment

--- backend/server.js
+++ backend/server.js
@@ -28,121 +28,121 @@ app.get('/users', async (req, res) => {
     }
 })
 
-/* =======================
-FIND USER BY ID
-======================= */
-// app.get('/api/findUser', async (req, res) => {
-//     try {
-//         const { id } = req.query
+// /* =======================
+// FIND USER BY ID
+// ======================= */
+// // app.get('/api/findUser', async (req, res) => {
+// //     try {
+// //         const { id } = req.query
 
-//         const user = await prisma.test.findUnique({
+// //         const user = await prisma.test.findUnique({
+// //             where: { id: parseInt(id) }
+// //         })
+
+// //         if (!user) {
+// //             return res.status(404).json({ error: 'User not found' })
+// //         }
+
+// //         res.json(user)
+
+// //     } catch (err) {
+// //         console.error(err)
+// //         res.status(500).json({ error: 'Server error' })
+// //     }
+// // })
+// app.get('/users/:id', async (req, res) => {
+//   try {
+
+//     const { id } = req.params
+
+//     const user = await prisma.test.findUnique({
+//       where: { id: parseInt(id) }
+//     })
+
+//     if (!user) {
+//       return res.status(404).json({ error: 'User not found' })
+//     }
+
+//     res.json(user)
+
+//   } catch (err) {
+//     console.error(err)
+//     res.status(500).json({ error: 'Server error' })
+//   }
+// })
+
+// /* =======================
+// CREATE USER
+// ======================= */
+// app.post('/api/users', async (req, res) => {
+//   try {
+
+//     const { id, name, email, phone } = req.body
+
+//     const newUser = await prisma.test.create({
+//       data: {
+//         id: parseInt(id),
+//         name,
+//         email,
+//         phone: parseInt(phone)
+//       }
+//     })
+
+//     res.json(newUser)
+
+//   } catch (err) {
+//     console.error(err)
+//     res.status(500).json({ error: 'Server error' })
+//   }
+// })
+
+// /* =======================
+// UPDATE USER
+// ======================= */
+// app.put('/api/users/:id', async (req, res) => {
+//   try {
+
+//     const { id } = req.params
+//     const { name, email, phone } = req.body
+
+//     const updatedUser = await prisma.test.update({
+//       where: { id: parseInt(id) },
+//       data: {
+//         name,
+//         email,
+//         phone: parseInt(phone)
+//       }
+//     })
+
+//     res.json(updatedUser)
+
+//   } catch (err) {
+//     console.error(err)
+//     res.status(500).json({ error: 'Server error' })
+//   }
+
+// })
+
+
+// /* =======================
+// DELETE USER
+// ======================= */
+// app.delete('/api/users/:id', async (req, res) => {
+//     try {
+
+//         const { id } = req.params
+
+//         await prisma.test.delete({
 //             where: { id: parseInt(id) }
 //         })
 
-//         if (!user) {
-//             return res.status(404).json({ error: 'User not found' })
-//         }
-
-//         res.json(user)
+//         res.json({ message: 'Deleted successfully' })
 
 //     } catch (err) {
 //         console.error(err)
 //         res.status(500).json({ error: 'Server error' })
 //     }
 // })
-app.get('/users/:id', async (req, res) => {
-  try {
-
-    const { id } = req.params
-
-    const user = await prisma.test.findUnique({
-      where: { id: parseInt(id) }
-    })
-
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' })
-    }
-
-    res.json(user)
-
-  } catch (err) {
-    console.error(err)
-    res.status(500).json({ error: 'Server error' })
-  }
-})
-
-/* =======================
-CREATE USER
-======================= */
-app.post('/api/users', async (req, res) => {
-  try {
-
-    const { id, name, email, phone } = req.body
-
-    const newUser = await prisma.test.create({
-      data: {
-        id: parseInt(id),
-        name,
-        email,
-        phone: parseInt(phone)
-      }
-    })
-
-    res.json(newUser)
-
-  } catch (err) {
-    console.error(err)
-    res.status(500).json({ error: 'Server error' })
-  }
-})
-
-/* =======================
-UPDATE USER
-======================= */
-app.put('/api/users/:id', async (req, res) => {
-  try {
-
-    const { id } = req.params
-    const { name, email, phone } = req.body
-
-    const updatedUser = await prisma.test.update({
-      where: { id: parseInt(id) },
-      data: {
-        name,
-        email,
-        phone: parseInt(phone)
-      }
-    })
-
-    res.json(updatedUser)
-
-  } catch (err) {
-    console.error(err)
-    res.status(500).json({ error: 'Server error' })
-  }
-
-})
-
-
-/* =======================
-DELETE USER
-======================= */
-app.delete('/api/users/:id', async (req, res) => {
-    try {
-
-        const { id } = req.params
-
-        await prisma.test.delete({
-            where: { id: parseInt(id) }
-        })
-
-        res.json({ message: 'Deleted successfully' })
-
-    } catch (err) {
-        console.error(err)
-        res.status(500).json({ error: 'Server error' })
-    }
-})
 
 const PORT = process.env.PORT || 5000
 
